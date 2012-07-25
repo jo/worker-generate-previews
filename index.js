3c1e4cb82eb622e30b79732a76110e0908591362
Johannes J. Schmidt <schmidt@netzmerk.com>
2012-07-25 18:51:31 +0200

    add fluent-ffmpeg

--- index.js
+++ index.js
@@ -5,16 +5,55 @@ var WorkerAttachments = require("worker-attachments/lib/WorkerAttachments");
 
 // example mimimal worker that checks every jpg or png image
 var processor = (function() {
-  var formats = ['jpg', 'png'];
+  var formats = ['mp4'],
+      ffmpeg = require("fluent-ffmpeg"),
+      _ = require("underscore");
 
   return {
     check: function(doc, name) {
       return formats.indexOf(name.toLowerCase().replace(/^.*\.([^\.]+)$/, '$1')) > -1;
     },
     process: function(doc, name, next) {
-      this._log(doc, 'found image: ' + name);
-      // do stuff...
-      next();
+      var tempdir = '/tmp';
+      var options = {
+        count: 9,
+        filename: doc._id + '-screenshot-%i',
+        timemarks: [ '10%', '20%', '30%', '40%', '50%', '60%', '70%', '80%', '90%' ]
+      };
+
+      // FIXME
+      // guess I need a prox stream to pipe the response to
+
+      // request image and send it to ffmpeg
+      request(this._urlFor(doc, name), _.bind(function(error, response, data) {
+        new ffmpeg({ source: data })
+          .withSize(this.config.size)
+          .takeScreenshots(options, tempdir, function(error, thumbnames) {
+            if (error) {
+              console.warn('Error creating video thumbnail:');
+              console.warn(error);
+              return next(error);
+            }
+
+            thumbnames.forEach(function(thumbname, i) {
+              var filename = tempdir + '/' + thumbname;
+
+              try {
+                doc._attachments['thumbs/' + i + '.jpg'] = {
+                  content_type: 'image/jpeg',
+                  data: fs.readFileSync(filename).toString('base64')
+                };
+                fs.unlinkSync(filename);
+              } catch(error) {
+                console.warn('Error creating video thumbnail:');
+                console.warn(error);
+                delete doc._attachments['thumbs/' + i + '.jpg'];
+              }
+            });
+
+            next();
+          });
+      }, this));
     }
   };
 })();
